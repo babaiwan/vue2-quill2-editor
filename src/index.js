657dacb44db740b709a1e9d3d9bbcdce4e33cda7
yxchen <2547062163@qq.com>
2024-10-08 16:03:30 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Vue2Quill2Editor from './vue2-quill2-editor/vue2-quill2-editor.vue';
-import epui from 'ep-ui';
 import vcolorpicker from 'vcolorpicker';
 
 const MyComponent = {
     install(Vue) {
         // 自动注册插件
-        Vue.use(epui);
         Vue.use(vcolorpicker);
 
         // 注册组件
